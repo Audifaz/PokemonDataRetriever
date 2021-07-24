b97d55051168f362e2111c023221caefc2322197
Audifaz <emilio.aguila@hotmail.com>
2021-07-24 09:55:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This small application will test how to retrieve from a WebApi JSON data and images.
+
+JSON data will be archived in a SQL database and images in folders.
+
+For this purpose, it will be used the pokemonApi 'https://pokeapi.co/'.
---
+++ retriever.py
@@ -0,0 +1,7 @@
+#TODO create virtual enviroment for the project
+#TODO retrieve data through http get request method
+#TODO retrieve images through http get request method
+#TODO storage data in Database
+#TODO storage images in subfolders
+#TODO storage local references to the images in the database
+
