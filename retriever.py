d9160f0eb9e9ce8bf6ef7705e4b3f2274730daaa
Audifaz <emilio.aguila@hotmail.com>
2021-07-24 10:50:50 -0500

    It is now possible to retrieve the complete data of many Pokemons at the same time and save them in a list

--- retriever.py
+++ retriever.py
@@ -3,4 +3,21 @@
 #TODO storage data in Database
 #TODO storage images in subfolders
 #TODO storage local references to the images in the database
+import requests
+import json
 
+def main():
+    #Retrive a JSON to 10 pokemons
+    r = requests.get('https://pokeapi.co/api/v2/pokemon?limit=10')
+    PreviewPokemons=r.json().get('results')
+    Pokemons=list()
+    #Get the complete Json for every Pokemon previously retrieved
+    for x in PreviewPokemons:
+        Pokemons.append((requests.get(x.get('url')).json()))
+    
+    for x in Pokemons:
+        print(x.get('name'))
+        print(x.get('base_experience'))
+
+if __name__ == "__main__":
+    main()
